65e841628a2072712c8f8bac046d6c7419cf0ca3
Phil Evans <pevans@paylocity.com>
2018-02-02 07:30:56 -0500

    removing router history

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-//console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Router} from 'react-router';
+import routes from './routes';
+import './styles/styles.css'; //Webpackd
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+
+
+render(
+    <Router  routes={routes}/>,
+    document.getElementById('app')
+);
