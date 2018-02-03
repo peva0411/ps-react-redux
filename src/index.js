e34c2abb0990ff02fb05916cf39b159177b87f81
Phil Evans <pevans@paylocity.com>
2018-02-03 11:38:44 -0500

    end of module 9, redux flow

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import {Router} from 'react-router';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import routes from './routes';
 import './styles/styles.css'; //Webpackd
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-
+const store = configureStore();
 
 render(
-    <Router  routes={routes}/>,
+    <Provider store={store}>
+        <Router  routes={routes}/>
+    </Provider>,
     document.getElementById('app')
 );
